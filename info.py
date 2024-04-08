cda97bf6454903c26b874f4377f809bfe170b128
Samuel <113779172+Dont-Know-Anythingg@users.noreply.github.com>
2024-04-08 15:57:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 # Assign FQDN based on environment variables and conditions
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if (not ON_HEROKU or getenv('FQDN')) else (APP_NAME + '.herokuapp.com')
-URL = "https://demo-gsmz.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://demo-gsmz.onrender.com/".format(FQDN, PORT)
+FQDN = str(getenv('FQDN', BIND_ADRESS)) if (not ON_HEROKU or getenv('FQDN')) else ((APP_NAME or '') + '.herokuapp.com')
+URL = "https://testzno-f6087b8c0b5b.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://testzno-f6087b8c0b5b.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'StreamBot'))
@@ -138,9 +138,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://mytestvj-1712ee43580d.herokuapp.com/".format(FQDN)
+    URL = "https://testzno-f6087b8c0b5b.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://mytestvj-1712ee43580d.herokuapp.com/".format(FQDN)
+    URL = "https://testzno-f6087b8c0b5b.herokuapp.com/".format(FQDN)
 
 
 
